4c00d548e75a19e87af7605685b8f9269debbc44
cbpark <cbpark@gmail.com>
2016-10-27 16:05:47 +0200

    Modify Course.Id, Course.Optional, Course.Validation

--- src/Course/Optional.hs
+++ src/Course/Optional.hs
@@ -1,12 +1,12 @@
-{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Course.Optional where
 
-import qualified Control.Applicative as A
-import qualified Control.Monad as M
-import Course.Core
-import qualified Prelude as P
+import           Course.Core
+
+import           Control.Monad (ap, liftM)
+import           Prelude       (Applicative (..), Functor (..), Monad (..))
 
 --  class Optional<A> {
 --    Optional(A a) {} // Full
@@ -23,35 +23,30 @@ bindOptional _ Empty    = Empty
 bindOptional f (Full a) = f a
 
 (??) :: Optional a -> a -> a
-Empty ?? d  = d
+Empty  ?? d = d
 Full a ?? _ = a
 
 (<+>) :: Optional a -> Optional a -> Optional a
 Empty <+> o = o
-k <+> _     = k
+k     <+> _ = k
 
 applyOptional :: Optional (a -> b) -> Optional a -> Optional b
-applyOptional f a = bindOptional (\f' -> mapOptional (\a' -> f' a') a) f
+applyOptional f a = bindOptional (`mapOptional` a) f
 
 twiceOptional :: (a -> b -> c) -> Optional a -> Optional b -> Optional c
 twiceOptional f = applyOptional . mapOptional f
 
 contains :: Eq a => a -> Optional a -> Bool
-contains _ Empty = False
+contains _ Empty    = False
 contains a (Full z) = a == z
 
-instance P.Functor Optional where
-  fmap =
-    M.liftM
+instance Functor Optional where
+    fmap = liftM
 
-instance A.Applicative Optional where
-  (<*>) =
-    M.ap
-  pure =
-    Full
+instance Applicative Optional where
+    (<*>) = ap
+    pure = Full
 
-instance P.Monad Optional where
-  (>>=) =
-    flip bindOptional
-  return =
-    Full
+instance Monad Optional where
+    (>>=) = flip bindOptional
+    return = Full
